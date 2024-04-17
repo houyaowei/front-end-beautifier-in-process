869c728f17f026bbf05f0c5d6bb97d0f693d902f
houyw <houyaowei@163.com>
2024-04-17 22:05:41 +0800

    doc&code update

--- code/rust-test-cases/basic/src/main.rs
+++ code/rust-test-cases/basic/src/main.rs
@@ -18,7 +18,12 @@ fn test_string() {
 
     println!("mut s1: {s1}")
 }
+
+fn test_add( a:i8, b:i8) -> i8 {
+    a+b
+}
 fn main() {
     test_string();
     test_variable();
+    println!("add func ,result: {:?}", test_add(3,6))
 }
